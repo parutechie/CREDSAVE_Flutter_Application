c8e43760d35e08c40642bd0e8381a932213bf310
Parvesh <49087655+parutechie@users.noreply.github.com>
2023-10-12 12:15:13 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/cred_save/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bingiz.cred_save
+
+import io.flutter.embedding.android.FlutterActivity
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+   override fun configureFlutterEngine(@NonNull flutterEngine: 
+     FlutterEngine) {
+     GeneratedPluginRegistrant.registerWith(flutterEngine)
+   }
+}
+
+
